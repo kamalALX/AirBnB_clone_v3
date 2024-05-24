c1974ee44d22a4f78118c280c12892471765f35b
KamalALX <kamalmiftah01@gmail.com>
2024-05-24 17:37:44 +0100

    fixing pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -87,6 +87,10 @@ class FileStorage:
     def count(self, cls=None):
         """ Return the number of objects in storage """
         if cls:
-            return len([obj for obj in self.__objects.values() if isinstance(obj, cls)])
+            return len(
+                    [obj for obj in self.__objects.values() if isinstance(
+                        obj, cls
+                        )]
+                    )
         else:
             return len(self.__objects)
