0495cbb99559f5e7d1fcaa9f751b5b7c25ec00d9
KamalALX <kamalmiftah01@gmail.com>
2024-05-25 17:28:57 +0100

    solving task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -22,11 +22,49 @@ def get_city(city_id):
         abort(404)
     return jsonify(city.to_dict())
 
-@app_views.route('/api/v1/cities/<city_id>', methods=['DELETE'])
-def delete_city():
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    """Creates a City."""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+    data = request.get_json()
+    if 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
+
+    new_city = City(**data)
+    new_city.state_id = state_id
+    storage.new(new_city)
+    storage.save()
+    return jsonify(new_city.to_dict()), 201
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    """Updates a City object."""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+    data = request.get_json()
+
+    ignored_keys = {'id', 'state_id', 'created_at', 'updated_at'}
+    for key, value in data.items():
+        if key not in ignored_keys:
+            setattr(city, key, value)
+
+    storage.save()
+    return jsonify(city.to_dict()), 200
