8dcc3aa698acb6fd9287319f2be6460b76274bf2
KamalALX <kamalmiftah01@gmail.com>
2024-05-26 01:25:12 +0100

    solving task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,6 +16,7 @@ def get_cities(state_id):
     city_list = [city.to_dict() for city in state.cities]
     return jsonify(city_list)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id):
@@ -23,6 +24,7 @@ def get_city(city_id):
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
                  strict_slashes=False)
@@ -34,41 +36,26 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-# @app_views.route('/states/<state_id>/cities', methods=['POST'])
-# def create_city(state_id):
-#     """Creates a City."""
-#     state = storage.get(State, state_id)
-#     if not state:
-#         abort(404)
-    
-#     if not request.is_json:
-#         return jsonify({"error": "Not a JSON"}), 400
-#     data = request.get_json()
-#     if 'name' not in data:
-#         return jsonify({"error": "Missing name"}), 400
 
-#     new_city = City(**data)
-#     setattr(new_city, 'state_id', state_id)
-#     storage.new(new_city)
-#     storage.save()
-#     return jsonify(new_city.to_dict()), 201
-
-@app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
-                 strict_slashes=False)
-def post_city(state_id):
-    """create a new city"""
-    state = storage.get("State", state_id)
-    if state is None:
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    """Creates a City."""
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    kwargs = request.get_json()
-    kwargs['state_id'] = state_id
-    city = City(**kwargs)
-    city.save()
-    return make_response(jsonify(city.to_dict()), 201)
+    
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+    data = request.get_json()
+    if 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
+
+    new_city = City(**data)
+    setattr(new_city, 'state_id', state_id)
+    storage.new(new_city)
+    storage.save()
+    return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'],
                  strict_slashes=False)
